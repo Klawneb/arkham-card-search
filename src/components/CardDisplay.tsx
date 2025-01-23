71f417fb3e26422261080d4d94abfc3823fff4a4
klwaneb <benjaminwalker222@gmail.com>
2025-01-23 20:51:10 +0000

    add generic card display

--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -19,11 +19,14 @@ export default function CardDisplay() {
         return <p>Loading...</p>
     }
 
-    return <div>
-        {
-            cards.data?.map((card) => (
-                <p>{card.name}</p>
-            ))
-        }
+    return <div className="grid grid-cols-[repeat(auto-fit,300px)] auto-rows-[420px] gap-4 justify-center">
+        {cards.data?.map((card, i) => (
+            <div
+                key={i}
+                className="bg-zinc-500 text-white w-[300px] h-[420px] rounded-xl shadow flex items-center justify-center text-center overflow-hidden"
+            >
+                <img loading={"lazy"} src={`https://arkhamdb.com${card.imagesrc}`} className="h-full w-full object-cover object-left"/>
+            </div>
+        ))}
     </div>
 }
