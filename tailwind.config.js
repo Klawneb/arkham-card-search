40ee528124ee5f4f38dbeb97b771eb131c0c68e3
Klawneb <benjaminwalker222@gmail.com>
2025-03-06 21:15:48 +0000

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      perspective: {
-        1000: "1000px"
-      }
+      fontFamily: {
+        arno: ["ArnoPro-Regular", "serif"],
+        teutonic: ["Teutonic", "serif"],
+        wolgast: ["Wolgast Script", "serif"],
+      },
     },
   },
-  plugins: [
-      require("tailwindcss-3d")({
-        legacy: true,
-      })
-  ],
-}
-
+};
