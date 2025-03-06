b210ae38d1b1ba9eadff304f67e211ddb7ca7848
klwaneb <benjaminwalker222@gmail.com>
2025-03-06 20:37:10 +0000

    slight modal improvements

--- src/components/CardModal.tsx
+++ src/components/CardModal.tsx
@@ -9,8 +9,27 @@ interface CardModalProps {
 
 const CardModal = ({ onClose, opened, card }: CardModalProps) => {
   return (
-    <Modal opened={opened} onClose={onClose} withCloseButton={false}>
-      {card?.name}
+    <Modal
+      opened={opened}
+      onClose={onClose}
+      withCloseButton={false}
+      centered
+      size={"auto"}
+      className="container"
+    >
+      <div className="grid grid-cols-[500px,auto,500px]">
+        <div>
+        </div>
+        <img
+          src={`https://arkhamdb.com${card?.imagesrc}`}
+          alt={`${card?.name} card art`}
+          className="h-[60vh]"
+        />
+        <div className="flex flex-col">
+          <h2 className="text-center text-4xl">{card?.name}</h2>
+          <p className="text-center text-xl">{card?.text}</p>
+        </div>
+      </div>
     </Modal>
   );
 };
