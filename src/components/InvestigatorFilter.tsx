017ad6f2ee4dbefeef064850e7ae32e4a3f6d6fa
Klawneb <benjaminwalker222@gmail.com>
2025-03-15 00:21:01 +0000

    improved investigator ui

--- src/components/InvestigatorFilter.tsx
+++ src/components/InvestigatorFilter.tsx
@@ -1,101 +1,147 @@
-import { Autocomplete, AutocompleteProps, ComboboxItem, OptionsFilter } from "@mantine/core";
-import { Card, FactionColors, Type } from "../types/api";
-import fuzzysort from "fuzzysort";
+import {
+  AspectRatio,
+  Autocomplete,
+  AutocompleteProps,
+  Combobox,
+  ComboboxItem,
+  OptionsFilter,
+  TextInput,
+  useCombobox,
+  Image,
+  Modal,
+} from "@mantine/core";
+import { Card, Faction, FactionColors, Type } from "../types/api";
 import { darkenHexColor } from "../lib/colors";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
+import { X } from "lucide-react";
 
 interface InvestigatorFilterProps {
   cards: Card[];
 }
 
-function fixExpansionName(name: string) {
-  let newName = name;
-
-  if (name.includes("Investigator Expansion")) {
-    newName = name.replace("Investigator Expansion", "");
+function getTextColor(faction: string) {
+  if (faction === "Seeker") {
+    return "text-stone-300";
   }
 
-  return newName;
+  if (faction === "Neutral") {
+    return "text-white";
+  }
+
+  return "text-stone-400";
 }
 
 const InvestigatorFilter = ({ cards }: InvestigatorFilterProps) => {
-  const [selectedInvestigator, setSelectedInvestigator] = useState("");
+  const comboBox = useCombobox({});
+  const [inputValue, setInputValue] = useState("");
+  const [selectedInvestigator, setSelectedInvestigator] = useState<Card | null>(null);
+  const [opened, handlers] = useDisclosure();
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  function getAutocompleteData(cards: Card[]) {
-    const investigatorMap = new Map<string, Array<{ value: string; label: string }>>();
+  function getInvestigatorByCode(code: string) {
+    return cards.find((card) => card.code === code);
+  }
+
+  function getInvestigatorData() {
+    const investigatorMap = new Map<string, Card[]>();
 
     cards.forEach((card) => {
-      if (!(card.type_code === Type.Investigator)) {
+      if (card.type_code != Type.Investigator) {
         return;
       }
 
       if (!investigatorMap.has(card.faction_name)) {
         investigatorMap.set(card.faction_name, []);
       }
-      investigatorMap.get(card.faction_name)?.push({
-        value: card.code,
-        label: card.name,
-      });
+
+      investigatorMap.get(card.faction_name)?.push(card);
     });
 
-    const groups: { group: string; items: Array<{ value: string; label: string }> }[] = [];
-    investigatorMap.forEach((items, group) => {
-      groups.push({ group, items });
-    });
-    return groups;
+    return Object.fromEntries(investigatorMap.entries());
   }
-  function getInvestigatorByCode(cards: Card[], code: string) {
-    return cards.find((card) => card.code === code);
-  }
-
-  const renderAutocompleteOption: AutocompleteProps["renderOption"] = ({ option }) => {
-    const investigator = getInvestigatorByCode(cards, option.value);
-    return (
-      <div className={`w-full px-1 rounded-md flex`}>
-        <p>{investigator?.name} </p>
-        <p className="mx-1">-</p>
-        <p className="text-stone-500">{fixExpansionName(investigator?.pack_name ?? "")}</p>
-      </div>
-    );
-  };
-
-  const optionsFilter: OptionsFilter = ({ options, search }) => {
-    if (!search) return options;
-
-    const typedOptions = options as { group: string; items: ComboboxItem[] }[];
-
-    return typedOptions
-      .map((group) => {
-        const filteredItems = group.items
-          .map((item) => {
-            const result = fuzzysort.single(
-              search.toLowerCase(),
-              item.label.toLowerCase() + getInvestigatorByCode(cards, item.value)?.pack_name
-            );
-            return result ? { ...item, score: result.score } : null;
-          })
-          .filter((item) => item !== null && item.score > 0.75)
-          .sort((a, b) => (a!.score ?? 0) - (b!.score ?? 0));
-
-        return {
-          group: group.group,
-          items: filteredItems as ComboboxItem[],
-        };
-      })
-      .filter((group) => group.items.length > 0);
-  };
 
   return (
-    <div className="m-2">
-      <Autocomplete
-        clearable
-        placeholder="Investigator name"
-        data={getAutocompleteData(cards)}
-        renderOption={renderAutocompleteOption}
-        filter={optionsFilter}
+    <div className="m-2 flex flex-col">
+      <Combobox
+        store={comboBox}
+        onOptionSubmit={(value) => {
+          setInputValue(getInvestigatorByCode(value)?.name ?? "");
+          setSelectedInvestigator(getInvestigatorByCode(value) ?? null);
+          comboBox.closeDropdown();
+          inputRef.current?.blur();
+        }}
+        classNames={{ groupLabel: "text-xl text-white" }}
       >
-        
-      </Autocomplete>
+        <Combobox.Target>
+          <TextInput
+            placeholder="Investigator name"
+            value={inputValue}
+            ref={inputRef}
+            onChange={(event) => {
+              setInputValue(event.target.value);
+              comboBox.openDropdown();
+              comboBox.updateSelectedOptionIndex();
+            }}
+            onClick={() => comboBox.openDropdown()}
+            onFocus={() => comboBox.openDropdown()}
+            onBlur={() => comboBox.closeDropdown()}
+            rightSection={
+              <button
+                className="hover:scale-125 hover:fill-white transition-all"
+                onClick={() => {
+                  setInputValue("");
+                  setSelectedInvestigator(null);
+                }}
+              >
+                <X width={20} />
+              </button>
+            }
+          />
+        </Combobox.Target>
+        <Combobox.Dropdown className="max-h-80 overflow-auto">
+          {Object.entries(getInvestigatorData()).map(([faction, investigators]) => {
+            const factionEnum = Faction[faction as keyof typeof Faction];
+            return (
+              <Combobox.Group
+                label={faction}
+                style={{ backgroundColor: darkenHexColor(FactionColors[factionEnum], 20) }}
+                className="text-white"
+              >
+                {investigators.map((investigator) => {
+                  if (!investigator.name.toLowerCase().includes(inputValue.toLowerCase())) {
+                    return;
+                  }
+
+                  return (
+                    <Combobox.Option
+                      className="flex truncate items-center"
+                      value={investigator.code}
+                    >
+                      <p>{investigator.name}</p>
+                      <p className="mx-1">-</p>
+                      <p className={`text-xs ${getTextColor(faction)}`}>{investigator.pack_name}</p>
+                    </Combobox.Option>
+                  );
+                })}
+              </Combobox.Group>
+            );
+          })}
+        </Combobox.Dropdown>
+      </Combobox>
+      {selectedInvestigator && (
+        <div className="flex flex-col">
+          <AspectRatio
+            ratio={7 / 5}
+            onClick={() => handlers.open()}
+            className="cursor-pointer my-2"
+          >
+            <Image src={`https://arkhamdb.com${selectedInvestigator?.imagesrc}`} />
+          </AspectRatio>
+        </div>
+      )}
+
+      <Modal opened={opened} onClose={handlers.close}></Modal>
     </div>
   );
 };
