fde42c06bdc954d64b2bd70eb6cfd99894e138ad
Klawneb <benjaminwalker222@gmail.com>
2025-03-21 17:47:33 +0000

    improve modals

--- src/components/Sidebar/AboutModal.tsx
+++ src/components/Sidebar/AboutModal.tsx
@@ -1,4 +1,5 @@
-import { Modal, Anchor, Text } from "@mantine/core";
+import { Modal, Anchor, Text, List } from "@mantine/core";
+import { InfoIcon } from "lucide-react";
 
 interface AboutModalProps {
   opened: boolean;
@@ -6,19 +7,35 @@ interface AboutModalProps {
 }
 const AboutModal = ({ opened, onClose }: AboutModalProps) => {
   return (
-    <Modal opened={opened} onClose={onClose} size={"auto"} title="About">
-      <div className="flex flex-col">
-        <Text>
-          Thanks to <Anchor href="https://arkhamdb.com/">ArkhamDB</Anchor> for their excellent API.
-        </Text>
-        <Text className="mt-6">
-          If you discover any bugs feel free to submit an {" "}
-          <Anchor href="https://github.com/Klawneb/arkham-card-search/issues">
-            issue on Github.
-          </Anchor>
-        </Text>
-      </div>
-    </Modal>
+    <Modal.Root opened={opened} onClose={onClose} size={"auto"}>
+      <Modal.Overlay />
+      <Modal.Content>
+        <Modal.Header>
+          <Modal.Title>
+            <div className="flex gap-2 items-center">
+              <InfoIcon className="w-7 h-7" />
+              <Text className="text-2xl font-semibold">About</Text>
+            </div>
+          </Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Header>
+        <Modal.Body>
+          <List className="list-disc" spacing={"lg"}>
+            <List.Item>
+              Thanks to <Anchor href="https://arkhamdb.com/">ArkhamDB</Anchor> for their excellent
+              API.
+            </List.Item>
+            <List.Item>All texts are copyrighted by Fantasy Flight Games.</List.Item>
+            <List.Item>
+              If you discover any bugs feel free to submit an{" "}
+              <Anchor href="https://github.com/Klawneb/arkham-card-search/issues">
+                issue on Github.
+              </Anchor>
+            </List.Item>
+          </List>
+        </Modal.Body>
+      </Modal.Content>
+    </Modal.Root>
   );
 };
 
--- src/components/Sidebar/SettingsModal.tsx
+++ src/components/Sidebar/SettingsModal.tsx
@@ -1,6 +1,6 @@
 import { Divider, Modal, Slider, Switch, Text, useMantineColorScheme } from "@mantine/core";
 import { useSettingsStore } from "../../lib/settings";
-import { MoonIcon, Proportions } from "lucide-react";
+import { MoonIcon, Proportions, SettingsIcon } from "lucide-react";
 
 interface SettingsModalProps {
   opened: boolean;
@@ -33,47 +33,61 @@ const marks = [
 const SettingsModal = ({ opened, onClose }: SettingsModalProps) => {
   const settingsStore = useSettingsStore();
   const { toggleColorScheme, colorScheme } = useMantineColorScheme({
-    keepTransitions: true
+    keepTransitions: true,
   });
 
   return (
-    <Modal opened={opened} onClose={onClose} size={"md"} title="Settings">
-      <div className="flex flex-col p-2">
-        <div className="flex flex-col">
-          <div className="flex gap-2">
-            <Proportions />
-            <Text fw={500} size="lg">
-              Card Size
-            </Text>
-          </div>
+    <Modal.Root opened={opened} onClose={onClose} size={"md"}>
+      <Modal.Overlay />
+      <Modal.Content>
+        <Modal.Header>
+          <Modal.Title>
+            <div className="flex gap-2 items-center">
+              <SettingsIcon className="w-7 h-7"/>
+              <Text className="text-2xl font-semibold">Settings</Text>
+            </div>
+          </Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Header>
+        <Modal.Body>
+          <div className="flex flex-col p-2">
+            <div className="flex flex-col">
+              <div className="flex gap-2">
+                <Proportions />
+                <Text fw={500} size="lg">
+                  Card Size
+                </Text>
+              </div>
 
-          <Text size="sm" c="dimmed">
-            Adjust the size of cards in the grid
-          </Text>
-          <Slider
-            value={settingsStore.cardSize}
-            onChange={settingsStore.setCardSize}
-            marks={marks}
-            restrictToMarks
-            size={"md"}
-            label={null}
-          />
-        </div>
-        <Divider className="mt-10" />
-        <div className="flex mt-4 items-center gap-2">
-          <MoonIcon />
-          <Text fw={500} size="lg">
-            Dark Mode:
-          </Text>
-          <Switch
-            size="lg"
-            onChange={toggleColorScheme}
-            checked={colorScheme === "dark"}
-            defaultChecked
-          />
-        </div>
-      </div>
-    </Modal>
+              <Text size="sm" c="dimmed">
+                Adjust the size of cards in the grid
+              </Text>
+              <Slider
+                value={settingsStore.cardSize}
+                onChange={settingsStore.setCardSize}
+                marks={marks}
+                restrictToMarks
+                size={"md"}
+                label={null}
+              />
+            </div>
+            <Divider className="mt-10" />
+            <div className="flex mt-4 items-center gap-2">
+              <MoonIcon />
+              <Text fw={500} size="lg">
+                Dark Mode:
+              </Text>
+              <Switch
+                size="lg"
+                onChange={toggleColorScheme}
+                checked={colorScheme === "dark"}
+                defaultChecked
+              />
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal.Content>
+    </Modal.Root>
   );
 };
 
